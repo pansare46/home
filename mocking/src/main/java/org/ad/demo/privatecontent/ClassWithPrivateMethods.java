05130fc75055526aec4351bad8d85533ca7d2795
Adil Anis <adil.anis@celeritio.com>
2020-06-23 05:41:18 +0000

    ClassWithPrivateMethods.java edited online with Bitbucket

--- mocking/src/main/java/org/ad/demo/privatecontent/ClassWithPrivateMethods.java
+++ mocking/src/main/java/org/ad/demo/privatecontent/ClassWithPrivateMethods.java
@@ -5,4 +5,7 @@ public class ClassWithPrivateMethods {
         private String printMessage(String message){
             return message;
         }
+        public String privateCall(String message){
+            return printMessage(message);
+        }
 }
